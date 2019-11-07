5785d9a063d4a266491364ac0e4338bdccda3b9a
daniilLS <w59920@student.wsiz.rzeszow.pl>
2019-11-07 18:44:00 +0100

    MathUtilities class created with one command

---
+++ ClassesNo3/BasicMethods/MathUtilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicMethods
+{
+    public static class MathUtilities
+    {
+        public static double solveliniarequetion(double a, double b)
+        {
+            return a == 0 ? 0  : -b / a; 
+            //if (a ==0)
+           
+            //    return 0;
+
+            //    return -b / a;
+            
+   
+        }
+    }
+}
---
+++ ClassesNo3/BasicMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine(MathUtilities.solveliniarequetion(3, 6));
+        }
+    }
+}
