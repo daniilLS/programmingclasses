eb80115d06817655c72256de85737b7f32663eea
W59920 <2023danil@gmail.com>
2019-11-15 18:31:40 +0100

    nothing

---
+++ ClassesNo3/TestOne/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TestOne
+{
+    class Program
+    {
+        public static void Main(string[] arg )
+
+        {
+            int a = 0;
+            if (  )
+
+
+             Console.WriteLine(" enter N");
+
+
+        }
+        
+        
+    }
+}
