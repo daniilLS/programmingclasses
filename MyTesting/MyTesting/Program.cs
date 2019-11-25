c25eec9ee4055c42b71cf232fafbc7fcd96871f5
daniilLS <2023danil@gmail.com>
2019-11-25 04:36:51 +0100

    new

---
+++ MyTesting/MyTesting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine(" give me the N");
+            
+          i=
+
+            
+            
+        }
+    }
+}
