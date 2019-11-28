359746737fb66bce0736f9b476b42e2532a4024c
daniiLS <2023danil@gmail.com>
2019-11-28 18:29:58 +0100

    string

--- ArraysAndStrings/StringClasses/Program.cs
+++ ArraysAndStrings/StringClasses/Program.cs
@@ -7,6 +7,33 @@ namespace StringClasses
         static void Main(string[] args)
         {
             //RecognizeGender();
+            //IsYourNameLong();
+            //string test="2,4,5,23,2";
+            // int[] testResult = ConvertToArray(test);
+            Console.WriteLine(ToSentence("thiS Sectence Has creppy CasTing"));
+        }
+        public static string ToSentence(string input)
+        {
+            string temp = input.ToLower();
+            string firstletter = temp.Substring(0, 1).ToUpper();
+            return firstletter + temp.Substring(1);
+            
+
+        }
+        public static int[] ConvertToArray(string input)
+        {
+         
+           string[] temp= input.Split(",");
+            int[] result = new int[temp.Length];
+            for (int i = 0; i < temp.Length; i++)
+            {
+                result[i] = int.Parse(temp[i]);
+            }
+            return result;
+        } 
+
+        private static void IsYourNameLong()
+        {
             Console.WriteLine("enter name");
             string name = Console.ReadLine();
             if (name.Length > 5)
