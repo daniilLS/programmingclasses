182dd5d67d30dfefd4ee56d7f1277e95518d254a
W59920 <W59920@RA117A-16>
2020-01-09 17:52:06 +0100

    game

---
+++ WinformsApps/Basicapp/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Basicapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startbutton = new System.Windows.Forms.Button();
+            this.massagelable = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startbutton
+            // 
+            this.startbutton.Location = new System.Drawing.Point(27, 28);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(107, 23);
+            this.startbutton.TabIndex = 0;
+            this.startbutton.Text = "New game";
+            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // massagelable
+            // 
+            this.massagelable.AutoSize = true;
+            this.massagelable.Location = new System.Drawing.Point(24, 87);
+            this.massagelable.Name = "massagelable";
+            this.massagelable.Size = new System.Drawing.Size(35, 13);
+            this.massagelable.TabIndex = 1;
+            this.massagelable.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 129);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 20);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Guess";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.massagelable);
+            this.Controls.Add(this.startbutton);
+            this.Name = "Form1";
+            this.Text = "Guessing game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Label massagelable;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WinformsApps/Basicapp/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basicapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            massagelable.Text = "press the button to start a new game";
+
+        }
+
+        private void startbutton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+    }
+}
