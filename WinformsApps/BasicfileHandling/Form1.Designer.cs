7c1044bff8fa45bb63062aaff663f425ff9df5aa
W59920 <W59920@RA117A-17>
2020-01-16 18:38:54 +0100

    textfile

---
+++ WinformsApps/BasicfileHandling/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace BasicfileHandling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.contentTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(12, 415);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(105, 415);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // contentTextBox
+            // 
+            this.contentTextBox.Location = new System.Drawing.Point(12, 13);
+            this.contentTextBox.Multiline = true;
+            this.contentTextBox.Name = "contentTextBox";
+            this.contentTextBox.Size = new System.Drawing.Size(763, 396);
+            this.contentTextBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.contentTextBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.loadButton);
+            this.Name = "Form1";
+            this.Text = "Notepad +";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox contentTextBox;
+    }
+}
+
---
+++ WinformsApps/BasicfileHandling/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BasicfileHandling
+{
+    public partial class Form1 : Form
+    {
+        private string _path = @"C:\Users\W59920\Desktop\Sample.txt";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+          contentTextBox.Text = File.ReadAllText(_path);
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(_path, contentTextBox.Text);
+        }
+    }
+}
