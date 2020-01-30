d4d565052e35cf56e465f430ccb935e856028139
W59920 <W59920@RA145B-21>
2020-01-30 18:27:49 +0100

    new

---
+++ ExamTryout/Utilities/stringutilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utilities
+{
+    public static class stringutilities
+    {
+        public static int ToWords(string input)
+        {
+            string[] words = input.Split(' ');
+            return words.Length;
+
+        }
+    }
+}
